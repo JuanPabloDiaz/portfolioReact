0644ecad272e0264dabd649968fea087c3e9b764
Juan Diaz <juanchodis@hotmail.com>
2022-12-07 10:30:58 -0500

    Live class 25/11

--- twitterReact/src/components/PublishTweet/index.jsx
+++ twitterReact/src/components/PublishTweet/index.jsx
@@ -1,44 +1,49 @@
-import React from "react";
-import styles from "./styles.module.css";
+import React from 'react'
+import styles from "./styles.module.css"
+
+import { saveInLocalStorage } from "../../utilities/saveInLocalStorage.js";
 
 import { FaImage } from "react-icons/fa";
-import { MdGpsFixed } from "react-icons/md";
+import { MdGpsFixed } from "react-icons/md"
 
 export const PublishTweet = () => {
   const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-  return (
-    <div className={styles.publishTweet}>
-      <img
-        src="https://picsum.photos/100"
-        alt="Profile pic"
-        className={`${styles.profilePic} img-default`}
-      />
-      <form className={styles.form}>
-        <textarea cols="30" rows="10" placeholder="Escribe un tweet"></textarea>
-        <div className={styles.footer}>
-          <ul className={styles.list}>
+    event.preventDefault()
+    const content = event.target.content.value
+    const user = {
+      tweet: content,
+      username: "LeoCode0",
+      name: "Leonardo Espinoza"
+    }
+
+    saveInLocalStorage("my-tweets", user)
+  }
+  return(
+    <div className={styles.publishTweet} >
+      <img src="https://picsum.photos/100" alt="Profile pic" className={`${styles.profilePic} img-default`} />
+      <form className={styles.form} >
+        <textarea cols="30" rows="10" placeholder='Escribe un tweet' >
+      <form className={styles.form} onSubmit={handleSubmit}  >
+        <textarea cols="30" rows="10" placeholder='Escribe un tweet' name='content' >
+
+        </textarea>
+        <div className={styles.footer} >
+          <ul className={styles.list} >
             <li>
-              <button onClick={() => console.log("Agregando imagen...")}>
+              <button onClick={() => console.log("Agregando imagen...")} >
                 <FaImage />
               </button>
             </li>
             <li>
-              <button onClick={() => console.log("Agregando ubicacion...")}>
+              <button onClick={() => console.log("Agregando ubicacion...")} >
                 <MdGpsFixed />
               </button>
             </li>
           </ul>
-          <button
-            onClick={handleSubmit}
-            type="submit"
-            className={styles.submit}
-          >
-            Tweetear
-          </button>
+          <button onClick={handleSubmit} type="submit" className={styles.submit} >Tweetear</button>
+          <button type="submit" className={styles.submit} >Tweetear</button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
