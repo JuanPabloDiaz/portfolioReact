a04ffdca964c10e36f08a0cbc205121df1c44a83
Juan Diaz <juanchodis@hotmail.com>
2022-12-24 14:16:00 -0500

    tik tok project

--- toktikProject/src/App.js
+++ toktikProject/src/App.js
@@ -1,9 +1,17 @@
-import { Video } from "./components/Video";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <div className="container">
-      <Video source={"https://picsum.photos/900"} />
+    <div className="App" >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
